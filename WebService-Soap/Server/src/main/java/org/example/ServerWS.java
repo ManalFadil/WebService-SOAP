9dc6a806acfc0386962cac188cf3c03ecca47f8c
ManalFadil <ma_fadil@etu.enset-media.ac.ma>
2024-12-18 08:27:38 +0100

    Ajout du dossier WebService-Soap

---
+++ WebService-Soap/Server/src/main/java/org/example/ServerWS.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import jakarta.xml.ws.Endpoint;
+
+public class ServerWS {
+
+    public static void main(String[] args) {
+        String url = "http://localhost:6655/";
+        Endpoint.publish(url, new BanqueService());
+        System.out.println("Web service est sur "+url);
+    }
+
+}
